9e81e8eddb78b678ea5671504bdcec8613857c98
Matheus Morcinek <matheus.morcinek@Matheuss-MacBook-Pro.local>
2020-10-05 18:21:52 -0300

    organizando projeto, criando funcionarios

---
+++ Funcionario/Diretor.js
@@ -0,0 +1,12 @@
+import { Funcionario } from "./Funcionario";
+
+import { Funcionario } from "./Funcionario";
+
+export class Diretor extends Funcionario{
+
+    constructor(nome, salario, cpf) {
+        super(nome, salario, cpf);
+        this._bonificacao = 2;
+    }
+
+}
---
+++ Funcionario/Gerente.js
@@ -0,0 +1,11 @@
+import { Funcionario } from "./Funcionario";
+
+import { Funcionario } from "./Funcionario";
+
+export class Gerente extends Funcionario {
+
+    constructor(nome, salario, cpf) {
+        super(nome, salario, cpf);
+        this._bonificacao = 1.1;
+    }
+}
